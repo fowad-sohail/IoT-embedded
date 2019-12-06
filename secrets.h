47e66c43b250dcc8d7e3d0ed6ea39276c6ba02bd
Fowad Sohail <sohailf7@students.rowan.edu>
2019-12-05 21:48:58 -0500

    add header files for thingspeak

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Rowan_IoT"    // replace MySSID with your WiFi network name
+#define SECRET_PASS ""  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 871140     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "H2L2CXV9RY0YPOVZ"   // replace XYZ with your channel write API Key
