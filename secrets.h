911ccfc443daa1fd243f041fdaf7be00a9696a54
Fowad Sohail <sohailf7@students.rowan.edu>
2019-12-05 21:55:32 -0500

    update with thingspeak credentials

--- secrets.h
+++ secrets.h
@@ -4,5 +4,5 @@
 #define SECRET_SSID "Rowan_IoT"    // replace MySSID with your WiFi network name
 #define SECRET_PASS ""  // replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 871140     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "H2L2CXV9RY0YPOVZ"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 922054     // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "FHA76MAQV6YL93KB"   // replace XYZ with your channel write API Key
